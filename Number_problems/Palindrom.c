4b929e24fae02cc6a1357df70d0b4dccbcc23213
Vishwajeet <vcnvim@gmail.com>
2022-05-09 16:00:52 +0530

    Palindrome_problem

---
+++ Number_problems/Palindrom.c
@@ -0,0 +1,30 @@
+/*
+
+   =============================
+     Author: Vishwajeet Kumar.
+    Email  : vcnvim@gmail.com
+   =============================
+
+Q. Program to check given number is palindrome number or not.
+ex.  12345 is equal to 54321 to or not.
+
+*/
+#include<stdio.h>
+
+int main(){
+    int num, x, rev= 0;
+    printf("Enter a number:) ");
+    scanf("%d",&num);
+    int	original_number = num;
+    while(num > 0){
+	x = num %10;
+	rev = (rev+x)*10;
+	num/=10;
+    }
+    rev/=10;
+    if(rev == original_number)
+	printf("Palindrome.\n");
+    else
+	printf("The number is not  palindrome type.\n");
+    return 0;
+}
